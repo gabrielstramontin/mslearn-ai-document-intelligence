1462b867de0fa700a3dff8fa5174028a371c88f2
Gabriel Santos Tramontin <145608943+gabrielstramontin@users.noreply.github.com>
2024-05-17 09:58:33 -0300

    Update setup.sh

--- Labfiles/03-composed-model/setup.sh
+++ Labfiles/03-composed-model/setup.sh
@@ -1,43 +1,44 @@
 #~!/bin/bash
 
-# Store color codes
+# Store Color Codes:
 NORMAL=$(tput sgr0)
 GREEN=$(tput setaf 2)
 
-# Set up the resource group
+# Set up the Resource Group:
 resourceGroupName=DocumentIntelligenceResources
 printf "${GREEN}Setting up the DocumentIntelligenceResources resource group. \n${NORMAL}"
 az group create --location westus2 --name DocumentIntelligenceResources
 
-# Create a name for the storage account
+# Create a name for the Storage Account:
 storageAccName=docintelstorage$((10000 + RANDOM % 99999))
 
-# Set up the Azure Storage account
+# Set up the Azure Storage Account:
 printf "${GREEN}Setting up the $storageAccName storage account. \n${NORMAL}"
 az storage account create --name $storageAccName --resource-group $resourceGroupName --kind StorageV2 --sku Standard_LRS
 
-# Get the connection string for the new storage account
+# Get the connection string for the new Storage Account:
 connectionString=$(az storage account show-connection-string --name $storageAccName --key key1 --resource-group $resourceGroupName --query connectionString)
 
-# Enable CORS on the storage account
+# Enable CORS on the Storage Account:
 az storage cors add --methods DELETE GET HEAD MERGE OPTIONS POST PUT --origins * --services b --allowed-headers * --max-age 200 --exposed-headers * --connection-string $connectionString
 
-# Create the storage containers
+# Create the Storage Containers:
 printf "${GREEN}Creating containers for the sample forms. \n${NORMAL}"
 az storage container create --account-name $storageAccName --name 1040examples --auth-mode login
 az storage container create --account-name $storageAccName --name 1099examples --auth-mode login
 az storage container create --account-name $storageAccName --name testdoc --auth-mode login
 
-# Upload the sample data
+# Upload the Sample Data:
 printf "${GREEN}Uploading the sample forms to the storage account. \n${NORMAL}"
 az storage blob upload-batch -d 1040examples --account-name $storageAccName --connection-string $connectionString -s "trainingdata/1040examples" --pattern *.pdf
 az storage blob upload-batch -d 1099examples --account-name $storageAccName --connection-string $connectionString -s "trainingdata/1099examples" --pattern *.pdf
 az storage blob upload-batch -d testdoc --account-name $storageAccName --connection-string $connectionString -s "trainingdata/TestDoc" --pattern *.pdf
 
-# Create the Forms Recognizer resource
+# Create the Forms Recognizer Resource:
 printf "${GREEN} Setting up the Document Intelligence resource. \n${NORMAL}"
 # First, purge it in case there's a recently deleted one
 SubID=$(az account show --query id --output tsv)
 az resource delete --ids "/subscriptions/${SubID}/providers/Microsoft.CognitiveServices/locations/westus2/resourceGroups/${resourceGroupName}/deletedAccounts/FormsRecognizer"
-# Now, create the new one
+
+# Now, create the new one:
 az cognitiveservices account create --kind FormRecognizer --location westus2 --name FormsRecognizer --resource-group $resourceGroupName --sku F0 --yes
